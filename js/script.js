5af2d05c664b492331695f1f00c7573c543fda7a
Read <460225561@qq.com>
2026-03-31 02:30:53 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // ========== 全局配置 ==========
-const API_BASE = 'https://solitudenook.top/api';
+const API_BASE = 'https://solitudenook.top';
 
 // ========== 全局变量与状态 ==========
 const navItems = document.querySelectorAll('.nav-item');
