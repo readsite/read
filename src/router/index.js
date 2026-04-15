0430e8fb1ff013ba9e14a3468a23be0d9eaeb58f
Read <460225561@qq.com>
2026-04-16 00:25:30 +0800

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -1,1 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
 
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../App.vue')
+  },
+  {
+    path: '/share',
+    name: 'Share',
+    component: () => import('../views/ShareView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
